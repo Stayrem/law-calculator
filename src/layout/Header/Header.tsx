d019eae582626ca42762257e1cdaf8c0bb905538
Dmitriy Spirin <spirin2205@yandex.ru>
2022-07-26 00:40:09 +0300

    login form

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Layout, Typography } from 'antd';
-import pathDict from '../../app/pathDict';
+import { Layout } from 'antd';
 import logo from '../../common/img/logo-white.svg';
 import css from './Header.scss';
+import LoginModal from '../../features/login/components/LoginModal/LoginModal';
 
 const { Header } = Layout;
 
@@ -10,6 +10,7 @@ const AppHeader = () => (
   <Header className={css.header}>
     <img className={css.logo} src={logo} alt="logo" />
     <div className="logo" />
+    <LoginModal />
   </Header>
 );
 
