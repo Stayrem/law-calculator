6ac7cc965d30d9947391feb6d5147df0c14c6aae
Dmitriy Spirin <spirin2205@yandex.ru>
2022-08-04 22:37:02 +0300

    added questions accordion

--- src/pages/Calculators/Calculators.tsx
+++ src/pages/Calculators/Calculators.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Breadcrumb, Typography } from 'antd';
+import {
+  Breadcrumb, Typography, Collapse, Row, Col,
+} from 'antd';
 import { useParams, Link } from 'react-router-dom';
-import { calcConfigs } from '../../constants';
+import { calcConfigs, calcQuestions } from '../../constants';
 import CalculatorsList from '../../features/calculators/components/CalculatorsList/CalculatorsList';
 import pathDict from '../../app/pathDict';
+import css from './Calculators.scss';
 
 const { Title } = Typography;
+const { Panel } = Collapse;
 type calcIdType = 'calc395' | 'calc317' | 'calcPenaltyKeyRate' | 'calcDoublePenaltyKeyRate' | 'calcPenaltyByContract';
 
 const Calculators = () => {
@@ -13,6 +17,7 @@ const Calculators = () => {
   const { calcId } = params;
   const Component = calcId ? calcConfigs[calcId].component : CalculatorsList;
   const title = calcId ? calcConfigs[calcId].title : 'Юридические калькуляторы';
+  const accordionList = calcId ? calcQuestions[calcId] : [];
   const renderBredCrumbs = () => (calcId
     ? (
       <Breadcrumb style={{ padding: '16px 0' }}>
@@ -30,11 +35,34 @@ const Calculators = () => {
       </Breadcrumb>
     ));
   return (
-    <div>
-      {renderBredCrumbs()}
-      <Title level={2}>{title}</Title>
-      <Component />
-    </div>
+    <>
+      <Row>
+        <Col span={24}>
+          <div>
+            {renderBredCrumbs()}
+            <Title level={2}>{title}</Title>
+          </div>
+        </Col>
+      </Row>
+      <Row className={css.calcWrapper}>
+        <Col span={24} xl={12}>
+          <Component />
+        </Col>
+        <Col span={24} lg={12}>
+          {accordionList.length && (
+          <div className={css.questionsWrapper}>
+            <Collapse key={1} accordion>
+              {accordionList.map((it) => (
+                <Panel header={it.title} key={it.title}>
+                  <p>{it.description}</p>
+                </Panel>
+              ))}
+            </Collapse>
+          </div>
+          )}
+        </Col>
+      </Row>
+    </>
   );
 };
 
